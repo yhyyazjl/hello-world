d4bf87f270f2070029f0402065c90a38f1596800
yehui <21395405@qq.com>
2016-09-25 19:20:04 +0800

    暂时不提交的

---
+++ src/com/yh/demo/HelloWorld2.java
@@ -0,0 +1,10 @@
+package com.yh.demo;
+
+/**
+ * Created by Administrator on 2016/9/25.
+ */
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("hello world22");
+    }
+}
