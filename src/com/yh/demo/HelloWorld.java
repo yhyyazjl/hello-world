6ea1221f879c76baf3d528feafb9aec12ef78c19
yehui <21395405@qq.com>
2016-09-25 18:37:42 +0800

    HelloWorld更新

--- src/com/yh/demo/HelloWorld.java
+++ src/com/yh/demo/HelloWorld.java
@@ -5,6 +5,6 @@ package com.yh.demo;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world1");
+        System.out.println("hello world11");
     }
 }
